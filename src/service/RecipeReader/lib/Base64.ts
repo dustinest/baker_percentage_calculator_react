a1def0d97d750c1eef59580d688f8965abf1f732
margus <margus.rebane@gmail.com>
2022-04-03 22:28:03 +0300

    Fixed the typo on == => ===

--- src/service/RecipeReader/lib/Base64.ts
+++ src/service/RecipeReader/lib/Base64.ts
@@ -1,7 +1,7 @@
 import {Buffer} from "buffer";
 
 export const base64Encode = (...values: (string | number)[]) => {
-    if (values.length == 1) {
+    if (values.length === 1) {
         return Buffer.from(values[0].toString(), 'utf8').toString('base64');
     }
     return Buffer.from(JSON.stringify(values), 'utf8').toString('base64');
